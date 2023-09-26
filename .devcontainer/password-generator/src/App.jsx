e040c7f120952fea076ec7082ab33054055359c2
sameer <38357131+sameerniswade@users.noreply.github.com>
2023-09-26 05:00:50 +0000

    install react and tailwind and clean starter code

---
+++ .devcontainer/password-generator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className='text-zinc-400 bg-gray-100'>
+    hello world
+  </div>
+  )
+}
+
+export default App
